ca1ba73a7ec3599203873ad052f9fd80d587d487
yanuabialwi <zhalxdev@gmail.com>
2023-02-10 09:05:23 +0700

    Update and rename adst-46860-3.js to adst-46860-4.js

--- adst-46860-4.js
+++ adst-46860-4.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 	atOptions = {
 		&#39;key&#39; : &#39;a2b68a56f8e117a4defc5c92c62b0ea5&#39;,
 		&#39;format&#39; : &#39;iframe&#39;,
@@ -9,4 +8,3 @@
 // <![CDATA[
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/a2b68a56f8e117a4defc5c92c62b0ea5/invoke.js"></scr' + 'ipt>');
 // ]]>
-</script>
