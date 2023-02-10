c1eec8bc4945550210cc45aa347268e42579cdde
yanuabialwi <zhalxdev@gmail.com>
2023-02-10 09:00:30 +0700

    Update and rename adst-46860.js to adst-46860-1.js

--- adst-46860-1.js
+++ adst-46860-1.js
@@ -6,5 +6,7 @@
 		'width' : 468,
 		'params' : {}
 	};
+// <![CDATA[
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/a2b68a56f8e117a4defc5c92c62b0ea5/invoke.js"></scr' + 'ipt>');
+// ]]>
 </script>
