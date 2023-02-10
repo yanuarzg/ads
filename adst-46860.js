a776012a0de1dbcbed7512f8b5c7cad9b8e18c8a
yanuabialwi <zhalxdev@gmail.com>
2023-02-10 08:55:00 +0700

    Create adst-46860.js

---
+++ adst-46860.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'a2b68a56f8e117a4defc5c92c62b0ea5',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/a2b68a56f8e117a4defc5c92c62b0ea5/invoke.js"></scr' + 'ipt>');
+</script>
